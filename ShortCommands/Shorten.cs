342dd2c3553e2633f2bf779f7e976701e967313c
Zaicon Kiroshu <kzaicon@yahoo.com>
2016-06-01 06:21:28 -0500

    API 1.24 Update (reverted from commit ccb4bb0aaf2a307f7fe5098845994303812f3650)

--- ShortCommands/Shorten.cs
+++ ShortCommands/Shorten.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 
 namespace ShortCommands
 {
-    [ApiVersion(1,24)]
+    [ApiVersion(1,23)]
     public class Shorten : TerrariaPlugin
     {
         public override string Name { get { return "ShortCommands"; } }
         public override string Author { get { return "Zaicon"; } }
         public override string Description { get { return "Enables live Shortcommands."; } }
-        public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+        public override Version Version { get { return new Version(1, 2, 6, 0); } }
 
         public static Config config = new Config();
         public string configPath = Path.Combine(TShock.SavePath, "ShortCommands.json");
